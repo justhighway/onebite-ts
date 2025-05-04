973ca6695a5329681405e99376b476f358792233
popo <hirouth@naver.com>
2025-05-05 00:13:10 +0900

    .

--- section03/src/ch3.ts
+++ section03/src/ch3.ts
@@ -1,1 +1,34 @@
 // 객체 간 타입 호환성
+// 객체에서는 조건(프로퍼티)가 더 적은 타입이 super 타입이 됨
+
+// super type
+type Animal = {
+  name: string;
+  color: string;
+};
+
+// sub type
+type Dog = {
+  name: string;
+  color: string;
+  breed: string;
+};
+
+let animal: Animal = {
+  name: "기린",
+  color: "yellow",
+};
+
+let dog: Dog = {
+  name: "돌돌이",
+  color: "white",
+  breed: "진도",
+};
+
+// up cast
+animal = dog;
+
+// down cast
+dog = animal;
+
+/* ------------------------ */
